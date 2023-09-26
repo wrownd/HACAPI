13b143d0fed38af1d719eb6b8af3c0ad00c4c16b
William Rownd <106162244+wrownd@users.noreply.github.com>
2023-09-26 10:32:46 -0500

    Update info.py
    Accepts School District selection for HAC23 in form of school ID.

--- api/info.py
+++ api/info.py
@@ -8,46 +8,58 @@ from api._lib.getRequestSession import getRequestSession
 
 class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    dic = dict(parse.parse_qsl(parse.urlsplit(self.path).query))
+    def do_GET(self):
+        query_string = parse.urlsplit(self.path).query
+        query_dict = dict(parse.parse_qsl(query_string))
 
-    username = dic["username"]
-    password = dic["password"]
+        # Extract the parameters from the URL
+        username = query_dict.get("username", "")
+        password = query_dict.get("password", "")
+        school_id = query_dict.get("sd", "380")  # Default to 380 if not provided
 
-    session = getRequestSession(username, password)
+        # Check if any of the required parameters is missing
+        if not username or not password:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write(json.dumps({"error": "Missing username or password"}).encode(encoding="utf_8"))
+            return
 
-    registrationPageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Registration.aspx").text
+        # Use the getRequestSession function with the provided parameters
+        session, school_name = getRequestSession(username, password, school_id)
 
-    parser =  BeautifulSoup(registrationPageContent, "lxml")
+        registrationPageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Registration.aspx").text
 
-    studentName = parser.find(id="plnMain_lblRegStudentName").text
-    studentBirthdate = parser.find(id="plnMain_lblBirthDate").text
-    studentCounselor = parser.find(id="plnMain_lblCounselor").text
-    studentCampus = parser.find(id="plnMain_lblBuildingName").text
-    studentGrade = parser.find(id="plnMain_lblGrade").text
-    totalCredits = 0
+        parser = BeautifulSoup(registrationPageContent, "lxml")
 
-    # Try to get the student id from the registration page
-    # If this fails, try to get the student id from the student schedule page
-    # Thank you @AniruddhAnand for this
-    try:
-      studentId = parser.find(id="plnMain_lblRegStudentID").text
-    except:
-      schedulePageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Classes.aspx")
-      parser =  BeautifulSoup(schedulePageContent, "lxml")
-      studentId = parser.find(id="plnMain_lblRegStudentID").text
+        studentName = parser.find(id="plnMain_lblRegStudentName").text
+        studentBirthdate = parser.find(id="plnMain_lblBirthDate").text
+        studentCounselor = parser.find(id="plnMain_lblCounselor").text
+        studentCampus = parser.find(id="plnMain_lblBuildingName").text
+        studentGrade = parser.find(id="plnMain_lblGrade").text
+        totalCredits = 0
 
-    self.send_response(200)
-    self.send_header('Content-type', 'application/json')
-    self.end_headers()
-    self.wfile.write(json.dumps({
-      "id": studentId,
-      "name": studentName,
-      "birthdate": studentBirthdate,
-      "campus": studentCampus,
-      "grade": studentGrade,
-      "counselor": studentCounselor,
-      "totalCredits": str(totalCredits)
-    }).encode(encoding="utf_8"))
+        # Try to get the student id from the registration page
+        # If this fails, try to get the student id from the student schedule page
+        try:
+            studentId = parser.find(id="plnMain_lblRegStudentID").text
+        except:
+            schedulePageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Classes.aspx")
+            parser = BeautifulSoup(schedulePageContent, "lxml")
+            studentId = parser.find(id="plnMain_lblRegStudentID").text
 
-    return
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response_data = {
+            "id": studentId,
+            "name": studentName,
+            "birthdate": studentBirthdate,
+            "campus": studentCampus,
+            "grade": studentGrade,
+            "counselor": studentCounselor,
+            "totalCredits": str(totalCredits),
+            "school_name": school_name
+        }
+        self.wfile.write(json.dumps(response_data).encode(encoding="utf_8"))
+
+        return
