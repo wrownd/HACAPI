abeb4936956965135fdffad2e3839c2cb0891cc1
William Rownd <106162244+wrownd@users.noreply.github.com>
2023-09-26 10:54:12 -0500

    Update info.py
    Bug fixes

--- api/info.py
+++ api/info.py
@@ -1,9 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 from bs4 import BeautifulSoup
 import json
-import lxml
 from urllib import parse
-
+from requests.exceptions import RequestException
 from api._lib.getRequestSession import getRequestSession
 
 class handler(BaseHTTPRequestHandler):
@@ -24,42 +23,62 @@ class handler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps({"error": "Missing username or password"}).encode(encoding="utf_8"))
             return
 
-        # Use the getRequestSession function with the provided parameters
-        session, school_name = getRequestSession(username, password, school_id)
-
-        registrationPageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Registration.aspx").text
-
-        parser = BeautifulSoup(registrationPageContent, "lxml")
-
-        studentName = parser.find(id="plnMain_lblRegStudentName").text
-        studentBirthdate = parser.find(id="plnMain_lblBirthDate").text
-        studentCounselor = parser.find(id="plnMain_lblCounselor").text
-        studentCampus = parser.find(id="plnMain_lblBuildingName").text
-        studentGrade = parser.find(id="plnMain_lblGrade").text
-        totalCredits = 0
-
-        # Try to get the student id from the registration page
-        # If this fails, try to get the student id from the student schedule page
         try:
-            studentId = parser.find(id="plnMain_lblRegStudentID").text
-        except:
-            schedulePageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Classes.aspx")
-            parser = BeautifulSoup(schedulePageContent, "lxml")
-            studentId = parser.find(id="plnMain_lblRegStudentID").text
+            # Use the getRequestSession function with the provided parameters
+            session, school_name = getRequestSession(username, password, school_id)
+            
+            registrationPageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Registration.aspx").text
 
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        response_data = {
-            "id": studentId,
-            "name": studentName,
-            "birthdate": studentBirthdate,
-            "campus": studentCampus,
-            "grade": studentGrade,
-            "counselor": studentCounselor,
-            "totalCredits": str(totalCredits),
-            "school_name": school_name
-        }
-        self.wfile.write(json.dumps(response_data).encode(encoding="utf_8"))
+            parser = BeautifulSoup(registrationPageContent, "lxml")
 
-        return
+            studentName = parser.find(id="plnMain_lblRegStudentName").text
+            studentBirthdate = parser.find(id="plnMain_lblBirthDate").text
+            studentCounselor = parser.find(id="plnMain_lblCounselor").text
+            studentCampus = parser.find(id="plnMain_lblBuildingName").text
+            studentGrade = parser.find(id="plnMain_lblGrade").text
+            totalCredits = 0
+
+            # Try to get the student id from the registration page
+            # If this fails, try to get the student id from the student schedule page
+            try:
+                studentId = parser.find(id="plnMain_lblRegStudentID").text
+            except:
+                schedulePageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Classes.aspx").text
+                parser = BeautifulSoup(schedulePageContent, "lxml")
+                studentId = parser.find(id="plnMain_lblRegStudentID").text
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            response_data = {
+                "id": studentId,
+                "name": studentName,
+                "birthdate": studentBirthdate,
+                "campus": studentCampus,
+                "grade": studentGrade,
+                "counselor": studentCounselor,
+                "totalCredits": str(totalCredits),
+                "school_name": school_name
+            }
+            self.wfile.write(json.dumps(response_data).encode(encoding="utf_8"))
+
+        except RequestException as e:
+            # Handle requests-related exceptions
+            self.send_response(500)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_response = {
+                "error": "Internal Server Error",
+                "message": str(e)  # Include the exception message for debugging
+            }
+            self.wfile.write(json.dumps(error_response).encode(encoding="utf_8"))
+        except Exception as e:
+            # Handle other exceptions
+            self.send_response(500)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_response = {
+                "error": "Internal Server Error",
+                "message": str(e)  # Include the exception message for debugging
+            }
+            self.wfile.write(json.dumps(error_response).encode(encoding="utf_8"))
