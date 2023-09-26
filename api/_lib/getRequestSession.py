3cc0d21a3693f82f616c3befc2bd6d0be8a317db
William Rownd <106162244+wrownd@users.noreply.github.com>
2023-09-26 10:36:55 -0500

    Update getRequestSession.py
    Override update, ignore branch.
    
    Adds support for School ID's in HAC23

--- api/_lib/getRequestSession.py
+++ api/_lib/getRequestSession.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
+import json
 
-def getRequestSession(username, password):
+def getRequestSession(username, password, school_id):
     requestSession = requests.session()
+
+    # Load the school data from the JSON file
+    with open('schools.json', 'r') as schools_file:
+        schools_data = json.load(schools_file)
+
+    # Get the school name based on the school_id
+    school_name = schools_data.get(school_id)
+
+    if school_name is None:
+        raise ValueError("Invalid school ID")
 
     loginScreenResponse = requestSession.get("https://hac23.esp.k12.ar.us/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f").text
 
-    parser =  BeautifulSoup(loginScreenResponse, "lxml")
+    parser = BeautifulSoup(loginScreenResponse, "lxml")
 
     requestVerificationToken = parser.find('input', attrs={'name': '__RequestVerificationToken'})["value"]
 
@@ -24,7 +35,7 @@ def getRequestSession(username, password):
         "__RequestVerificationToken" : requestVerificationToken,
         "SCKTY00328510CustomEnabled" : "False",
         "SCKTY00436568CustomEnabled" : "False",
-        "Database" : "380",
+        "Database" : school_id,
         "VerificationOption" : "UsernamePassword",
         "LogOnDetails.UserName": username,
         "tempUN" : "",
@@ -38,4 +49,4 @@ def getRequestSession(username, password):
         headers=requestHeaders
     )
 
-    return requestSession
+    return requestSession, school_name
