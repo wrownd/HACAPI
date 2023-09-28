58957d7050d217c40d0ee9d905b1e65919a0d85b
William Rownd <106162244+wrownd@users.noreply.github.com>
2023-09-27 21:40:01 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,12 +2,11 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print(f"REQUEST IP: {self.client_address[0]}")
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"hacapi | v1.0")
+        self.wfile.write("""hacapi | v1.0""".encode(encoding="utf_8"))
 
-if __name__ == "__main__":
-    from http.server import HTTPServer
-    server = HTTPServer(('0.0.0.0', 8080), handler)
-    server.serve_forever()
+        return
