35573010783e272c6d8b2fd8f8304d64b1fad37d
William Rownd <106162244+wrownd@users.noreply.github.com>
2023-09-27 22:09:21 -0500

    Update info.py

--- api/info.py
+++ api/info.py
@@ -1,77 +1,84 @@
-from http.server import BaseHTTPRequestHandler
-from bs4 import BeautifulSoup
 import json
 from urllib import parse
+from bs4 import BeautifulSoup
 from requests.exceptions import RequestException
 from api._lib.getRequestSession import getRequestSession
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path.startswith("/api/info"):
-            query = parse.parse_qs(parse.urlsplit(self.path).query)
-            username = query.get("username", [""])[0]
-            password = query.get("password", [""])[0]
-            school_id = query.get("sd", ["380"])[0]
+def handler(request):
+    # Extract the parameters from the query string
+    query_string = request.query
+    username = query_string.get("username", "")
+    password = query_string.get("password", "")
+    school_id = query_string.get("sd", "380")
 
-            if not username or not password:
-                response_data = {"error": "Missing username or password"}
-                self.send_response(400)
-                self.send_header("Content-Type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(response_data).encode("utf-8"))
-                return
+    # Check if any of the required parameters is missing
+    if not username or not password:
+        response_data = {"error": "Missing username or password"}
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(response_data),
+        }
 
-            try:
-                # Call the getRequestSession function to obtain the session and school name
-                session, school_name = getRequestSession(username, password, school_id)
+    try:
+        # Call the getRequestSession function to obtain the session and school name
+        session, school_name = getRequestSession(username, password, school_id)
 
-                # Simulate the response for the registration page content
-                registrationPageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Registration.aspx").text
-                parser = BeautifulSoup(registrationPageContent, "lxml")
+        # Simulate the response for the registration page content
+        registrationPageContent = session.get("https://hac23.esp.k12.ar.us/HomeAccess/Content/Student/Registration.aspx").text
+        parser = BeautifulSoup(registrationPageContent, "lxml")
 
-                studentName = parser.find(id="plnMain_lblRegStudentName").text
-                studentBirthdate = parser.find(id="plnMain_lblBirthDate").text
-                studentCounselor = parser.find(id="plnMain_lblCounselor").text
-                studentCampus = parser.find(id="plnMain_lblBuildingName").text
-                studentGrade = parser.find(id="plnMain_lblGrade").text
-                totalCredits = 0
+        studentName = parser.find(id="plnMain_lblRegStudentName").text
+        studentBirthdate = parser.find(id="plnMain_lblBirthDate").text
+        studentCounselor = parser.find(id="plnMain_lblCounselor").text
+        studentCampus = parser.find(id="plnMain_lblBuildingName").text
+        studentGrade = parser.find(id="plnMain_lblGrade").text
+        totalCredits = 0
 
-                response_data = {
-                    "id": "studentId",
-                    "name": studentName,
-                    "birthdate": studentBirthdate,
-                    "campus": studentCampus,
-                    "grade": studentGrade,
-                    "counselor": studentCounselor,
-                    "totalCredits": str(totalCredits),
-                    "school_name": school_name,
-                }
-                self.send_response(200)
-                self.send_header("Content-Type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(response_data).encode("utf-8"))
+        response_data = {
+            "id": "studentId",
+            "name": studentName,
+            "birthdate": studentBirthdate,
+            "campus": studentCampus,
+            "grade": studentGrade,
+            "counselor": studentCounselor,
+            "totalCredits": str(totalCredits),
+            "school_name": school_name,
+        }
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(response_data),
+        }
 
-            except RequestException as e:
-                # Handle requests-related exceptions
-                error_response = {
-                    "error": "Internal Server Error",
-                    "message": str(e),  # Include the exception message for debugging
-                }
-                self.send_response(500)
-                self.send_header("Content-Type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(error_response).encode("utf-8"))
+    except RequestException as e:
+        # Handle requests-related exceptions
+        error_response = {
+            "error": "Internal Server Error",
+            "message": str(e),  # Include the exception message for debugging
+        }
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(error_response),
+        }
 
-            except Exception as e:
-                # Handle other exceptions
-                error_response = {
-                    "error": "Internal Server Error",
-                    "message": str(e),  # Include the exception message for debugging
-                }
-                self.send_response(500)
-                self.send_header("Content-Type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(error_response).encode("utf-8"))
-        else:
-            self.send_response(404)
-            self.end_headers()
+    except Exception as e:
+        # Handle other exceptions
+        error_response = {
+            "error": "Internal Server Error",
+            "message": str(e),  # Include the exception message for debugging
+        }
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(error_response),
+        }
