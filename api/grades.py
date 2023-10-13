75650dfea382b53a0490b572a5422a6549d22dca
William Rownd <106162244+wrownd@users.noreply.github.com>
2023-10-12 21:37:51 -0500

    Update grades.py

--- api/grades.py
+++ api/grades.py
@@ -50,14 +50,10 @@ class handler(BaseHTTPRequestHandler):
                 for hc in headerContainer:
                     parser = BeautifulSoup(f"<html><body>{hc}</body></html>", "lxml")
                     
-                    # Extract the course name and code from sg-header-heading
-                    name_with_code = parser.find("a", "sg-header-heading").text.strip()
-                    name, code = name_with_code.split('-', 1)
-                    name = name.strip()
-                    code = code.strip() if code else ''
+                    # Extract the course name from sg-header-heading
+                    name = parser.find("a", "sg-header-heading").text
                     
                     newCourse["name"] = name
-                    newCourse["code"] = code
                     
                     grade_span = parser.find("span", "sg-header-heading sg-right").text.strip()
                     if "9 Weeks Grade " in grade_span:
